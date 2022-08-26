4860100598e014f139fc820e7204d9c97c0d11fc
Shubham Mandan <66783672+mandanshubham@users.noreply.github.com>
2022-08-26 18:38:11 +0530

    Create CountAllPalindromicSubsequence.cpp

---
+++ Dynamic Programming/Subsequence/CountAllPalindromicSubsequence.cpp
@@ -0,0 +1,54 @@
+// Link : https://www.geeksforgeeks.org/count-palindromic-subsequence-given-string/
+
+// Input : str = "abcd"
+// Output : 4
+// Explanation :- palindromic  subsequence are : "a" ,"b", "c" ,"d" 
+
+// Input : str = "aab"
+// Output : 4
+// Explanation :- palindromic subsequence are :"a", "a", "b", "aa"
+
+// Input : str = "aaaa"
+// Output : 15
+
+
+// C++ program to counts Palindromic Subsequence in a given String using recursion
+#include <bits/stdc++.h>
+using namespace std;
+
+int n, dp[1000][1000];
+string str = "abcb";
+
+// Function return the total
+// palindromic subsequence
+int countPS(int i, int j)
+{
+    if (i > j)
+        return 0;
+
+    if (dp[i][j] != -1)
+        return dp[i][j];
+
+    if (i == j)
+        return dp[i][j] = 1;
+
+    else if (str[i] == str[j])
+        return dp[i][j] = 1 + countPS(i + 1, j)
+                            + countPS(i, j - 1);
+    
+    else
+        return dp[i][j] = countPS(i + 1, j)
+                        + countPS(i, j - 1)
+                        - countPS(i + 1, j - 1);
+}
+
+// Driver code
+int main()
+{
+    memset(dp, -1, sizeof(dp));
+    n = str.size();
+    cout << "Total palindromic subsequence are : "
+        << countPS(0, n - 1) << endl;
+    return 0;
+}
+// this code is contributed by Kushdeep Mittal
